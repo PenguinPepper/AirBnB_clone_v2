a3ea4148e7df6a31bcc8c8e14bf5c84aa5f4d527
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-08 16:57:59 -0700

    Add temp files for task 1 and 2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive
+from the contents of the web_static folder
+"""
+
+
+def do_pack():
+    """
+    do pack function that creates an archieve
+    """
+    print('.')
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes a archieve to a server
+"""
+
+
+def do_deploy(archive_path):
+    """
+    function sends takes in an archieve path and sends it to a server
+    """
+    if not archive_path:
+        return False
+    return True
