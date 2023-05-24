a065659c090c18a5a27003b9d21709741f1bc9a5
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-24 16:43:14 -0700

    Add route that accepts arguments

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module contains script that starts a flask application
+
+Web application listesns on port 5000
+"""
+
+
+from flask import Flask, escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    ''''Returns text for home route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Return hbnb text'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    '''Return a special message for C'''
+    return 'C'.format(escape(text))
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
