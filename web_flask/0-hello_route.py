4743bb08f9613855323e5fa128367506f05a96f3
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-24 16:05:05 -0700

    Add file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ This script routes a simple web server
 
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.rout('/')
-def hello_hbnb():
+@app.route('/')
+def index():
     '''Function that returns text for home route'''
     return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
