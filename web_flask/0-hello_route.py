0965d0c6226e039e027b78d742c63c1d8f1eb227
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-05-24 10:01:58 -0700

    Add extra libe in doc string

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""
-This module contains a script to start a flask application
+"""This module contains a script to start a flask application
+
+This script routes a simple web server 
 """
 
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.rout('/')
