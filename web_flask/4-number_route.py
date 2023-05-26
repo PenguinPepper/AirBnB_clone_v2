e653e90b23f6c5874ce4138fcfbd7da6be835a36
Elizbeth Pfachi <testlolabtest@gmail.com>
2023-05-26 12:11:06 +0000

    Add script with route to /number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module contains script that starts a flask application
+
+Web application listesns on port 5000
+"""
+
+
+from flask import Flask, escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    ''''Returns text for home route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Return hbnb text'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    '''Return a special message for C'''
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    '''Return a special message for Python'''
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''Return n if it is an integer'''
+    return '{} is a number'.format(escape(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
